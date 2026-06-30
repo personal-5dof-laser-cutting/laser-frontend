3ea4e6ba0bbbe0183fe86c2630163f4f3f094d9b
corvinkoegler <corvin.koegler.hpi@gmail.com>
2026-06-30 23:11:58 +0200

    added toolbar component

---
+++ src/components/FloatingSidemenu.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="menu-container">
+    <form>
+  <p>
+    <select id="material-selector">
+      <option value="plywood">Plywood</option>
+    </select>
+  </p>
+</form>
+  </div>
+</template>
+
+<style scoped>
+.menu-container {
+ position: absolute;
+  top: 20px;
+  right: 20px;
+  z-index: 10;
+  display: flex;
+  flex-flow: column;
+  flex-grow: 1;
+  gap: 12px;
+
+  width: 300px;
+  padding: 15pt;
+  margin-left: auto;
+
+  overflow: hidden;
+
+  background: black;
+}
+
+#material-selector {
+  width: 100%;
+  background: var(--color-background-mute);
+  color: var(--color-text);
+  padding: 5pt;
+  border: 1pt solid var(--color-border);
+  border-radius: 5pt;
+}
+</style>
--- src/components/SVGCanvas.vue
+++ src/components/SVGCanvas.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import CanvasItem from '@/components/CanvasItem.vue'
+import CanvasToolbar from '@/components/CanvasToolbar.vue'
+import FloatingSidemenu from '@/components/FloatingSidemenu.vue'
 import { useProjectStore } from '@/stores/useProjectStore'
 import { useCutterStore } from '@/stores/useCutterStore'
 import { useStore } from '@/stores/useStore'
 
-import uploadIconUrl from '@/assets/icons/upload_file.svg'
-import resetBoxIconUrl from '@/assets/icons/reset_focus.svg'
-import deleteIconUrl from '@/assets/icons/delete.svg'
-import copyIconUrl from '@/assets/icons/copy.svg'
-import downloadIconUrl from '@/assets/icons/download.svg'
-
 const projectStore = useStore(useProjectStore)
-const { uploadSvg, updateSvg, removeSvg, selectSvg, duplicateSvg, resetProject } = useProjectStore.getState()
+const { uploadSvg, updateSvg, removeSvg, selectSvg, duplicateSvg, resetProject } =
+  useProjectStore.getState()
 const svgs = computed(() => projectStore.value.svgs)
 const selectedId = computed(() => projectStore.value.selectedId)
 
@@ -27,18 +24,6 @@ const pan = ref({ x: 0, y: 0 })
 const scale = ref(1)
 const isDragging = ref(false)
 const dragStart = ref({ mouseX: 0, mouseY: 0, panX: 0, panY: 0 })
-
-const duplicateActiveSvg = () => {
-  if (!selectedId.value) return
-
-  duplicateSvg(selectedId.value)
-}
-
-const removeActiveSVG = () => {
-  if (!selectedId.value) return
-
-  removeSvg(selectedId.value)
-}
 
 const getBaseSVGPosition = (evt: MouseEvent) => {
   if (!svgRef.value) return { x: 0, y: 0 }
@@ -159,29 +144,8 @@ const exportToSvg = () => {
 
 <template>
   <div class="svg-canvas-container">
-    <div class="toolbar">
-      <label class="icon-btn" title="Upload SVG">
-        <img :src="uploadIconUrl" class="btn-icon" alt="Upload Icon" />
-        <input type="file" accept=".svg" @change="uploadSvg" hidden />
-      </label>
-
-      <button class="icon-btn" title="Reset Canvas" @click="resetProject">
-        <img :src="resetBoxIconUrl" class="btn-icon" alt="Reset Canvas Icon" />
-      </button>
-
-      <button class="icon-btn" title="Delete Element" @click="removeActiveSVG" v-if="selectedId">
-        <img :src="deleteIconUrl" class="btn-icon" alt="Delete Element" />
-      </button>
-
-      <button class="icon-btn" title="Duplicate Element" @click="duplicateActiveSvg" v-if="selectedId">
-        <img :src="copyIconUrl" class="btn-icon" alt="Duplicate Element" />
-      </button>
-
-      <button class="icon-btn" title="Export Project" @click="exportToSvg">
-        <img :src="downloadIconUrl" class="btn-icon" alt="Export Project" />
-      </button>
-    </div>
-
+    <CanvasToolbar />
+    <FloatingSidemenu />
     <svg
       viewBox="-50 -50 450 450"
       class="root-svg"
@@ -201,10 +165,10 @@ const exportToSvg = () => {
 
       <g :transform="`translate(${pan.x}, ${pan.y}) scale(${scale})`" ref="viewportRef">
         <rect
-          x="-20000"
-          y="-20000"
-          width="40000"
-          height="40000"
+          x="-200000"
+          y="-200000"
+          width="400000"
+          height="400000"
           fill="url(#grid-pattern)"
           @click="selectSvg(null)"
         />
@@ -261,39 +225,5 @@ const exportToSvg = () => {
   stroke-width: 1pt;
   vector-effect: non-scaling-stroke;
   stroke-dasharray: 5 5;
-}
-
-.toolbar {
-  position: absolute;
-  top: 16px;
-  left: 16px;
-  z-index: 10;
-  display: flex;
-  flex-flow: row;
-  gap: 12px;
-}
-
-.icon-btn {
-  background-color: var(--color-background-soft);
-  border: 1px solid grey;
-  border-radius: 5px;
-  color: var(--color-text);
-  cursor: pointer;
-  width: 48pt;
-  height: 48pt;
-  padding: 8pt;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.icon-btn:hover {
-  border-color: var(--color-border-hover);
-}
-
-.btn-icon {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
 }
 </style>
