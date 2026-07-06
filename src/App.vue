d034a94c1d79515a483c97b09e724f094f9b4659
corvinkoegler <corvin.koegler.hpi@gmail.com>
2026-07-06 14:20:13 +0200

    added ws connection to project root

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useWebsocketStore } from '@/stores/useWebsocketStore'
+import { useStore } from '@/stores/useStore'
+
+const websocketStore = useStore(useWebsocketStore)
+const connect = (url: string) => useWebsocketStore.getState().connect(url)
+connect("ws://127.0.0.1:8000/ws/main")
 </script>
 
 <template>
